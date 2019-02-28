71d6d4f001bf00eb81e45555437d3903330b1679
Liam Csiffary <47784162+s299776@users.noreply.github.com>
2019-02-28 08:20:06 -0500

    Touch&React

--- Touch&ReactLiamC/main.lua
+++ Touch&ReactLiamC/main.lua
@@ -1,7 +1,16 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: TouchAndReact
+-- Name: Your Name
+-- Course: ICS2O/3C
+-- This program does something when I click on the button.
 
--- Your code here
+-- set background color
+display.setDefault ("background", 1, 0, 1)
+
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- create blue button, set its position and make it visible
+local blueButton = display.newImageRect("Images/playButton.png", 200, 200)
+
+blueButton.x = 512
+blueButton.y = 375
