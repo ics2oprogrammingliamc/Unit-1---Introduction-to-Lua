a87b06fdb5f0386ca22c63b4f38001cdb2e81ee7
Liam <liam.csiffary@ocsbstudent.ca>
2019-03-20 17:09:12 -0400

    Touch&React

--- Touch&ReactLiamC/main.lua
+++ Touch&ReactLiamC/main.lua
@@ -10,10 +10,36 @@ display.setDefault ("background", 1, 0, 1)
 display.setStatusBar(display.HiddenStatusBar)
 
 -- create blue button, set its position and make it visible
-local blueButton = display.newImageRect("Images/playButton.png", 200, 200)
+local blueButton = display.newImageRect("Images/playButton.png", 198, 96)
 blueButton.x = display.contentWidth/2
 blueButton.y = display.contentHeight/2
 blueButton.isVisible = true
 
 -- create red button, set its position and make it invisible
-local redButton = display.newImageRect("Images/")
+local redButton = display.newImageRect("Images/playButton.png", 198, 96)
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+blueButton.isVisible = false
+
+-- create text object, set its position and make it invisible
+local textObject = display.newText ("Clicked!", 0, 0, nil, 50)
+textObject.x = display.contentHeight/3
+textObject.y = display.contentWidth/2
+textObject:setTextColor(0,0,0)
+textObject.isVisible = false
+
+local function BlueButtonListener(touch)
+	if (touch.phase == "began") then
+		blueButton.isVisible = false
+		redButton.isVisible = true
+		textObject.isVisible = true
+	end
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+	end
+end
+
+blueButton:addEventListener("touch", BlueButtonListener)
